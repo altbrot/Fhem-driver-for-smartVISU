e4be976813756ff212177602015c3fba824a5abf
altbrot <altbrot@gmx.at>
2014-12-10 02:24:16 +0100

    Update io_fhem.php

--- io_fhem.php
+++ io_fhem.php
@@ -18,6 +18,7 @@ require_once '../lib/includes.php';
  */   
 class driver_json
 {
+    var $cmd = '';
     var $item = '';
     var $val = '';
    
@@ -26,6 +27,7 @@ class driver_json
     */ 
     public function __construct($request)
     {
+        $this->cmd = $request['cmd'];
         $this->item = explode(',', $request['item']);
 	$this->val = $request['val'];
     }
@@ -40,7 +42,7 @@ class driver_json
         
         foreach ($this->item as $item)
         {
-            $response = $this->sendReadCmd($item);
+            $response = $this->sendReadCmd(urlencode($this->parseReadCmd($item)));
             if($response !== '')
             {
                 $res[$item] = $response;
@@ -49,6 +51,34 @@ class driver_json
         return $res;
     }
     
+    public function readPlot()
+    {
+        $res = [];     
+        
+        if (count($this->item) > 0)
+        {
+            $response = $this->sendReadCmd(urlencode($this->parseReadPlotCmd($this->item[0], $this->val)));
+            if($response !== '')
+            {
+                $resarr = array();
+                $lines = explode("\n", rtrim($response, "\n"));
+                for($i = 0; $i < count($lines); $i++)
+                {
+                    $items = explode(" ", $lines[$i]);
+                    $item = [];
+                    $date = DateTime::createFromFormat("Y-m-d_H:i:s", $items[0]);
+                    if ($date){
+                        array_push($item, $date->format('U') * 1000);
+                        array_push($item, floatval($items[1]));
+                        array_push($resarr, $item);                        
+                    }
+                }
+                $res[$this->item[0]] = $resarr;
+            }           
+        }
+        
+        return $res;
+    }
     
     /** 
     * Write to bus
@@ -59,7 +89,7 @@ class driver_json
         
         if (count($this->item) > 0)
         {
-            $this->sendWriteCmd($this->item[0]);
+            $this->sendWriteCmd(urlencode($this->parseWriteCmd($this->item[0], $this->val)));
             $res[$this->item[0]] = $this->val;            
         }
         
@@ -72,26 +102,28 @@ class driver_json
     public function sync()
     {        
         $ret = array();
-	   
-        // write if a value is given
-        if ($this->val != '')
+	
+        switch ($this->cmd)
         {
-            $ret = $this->write();        
-        }
-        else
-        {
-            $ret = $this->read();
-        }
+            case 'read':
+                $ret = $this->read();
+                break;
+            case 'write':
+                $ret = $this->write();
+                break;
+            case 'plot':
+                $ret = $this->readPlot();
+        }       
                         
         return json_encode($ret);
     }
     
-    private function sendWriteCmd($item)
+    private function sendWriteCmd($cmd)
     {
         $host = config_driver_address;
         $port = config_driver_port;
 
-        $url = $host.":".$port."/fhem?cmd=".urlencode($this->parseWriteCmd($item, $this->val))."&XHR=1";
+        $url = $host.":".$port."/fhem?cmd=".$cmd."&XHR=1";
 
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -99,13 +131,13 @@ class driver_json
         curl_close($curl); 
     }
     
-    private function sendReadCmd($item)
+    private function sendReadCmd($cmd)
     {
         $host = config_driver_address;
         $port = config_driver_port;
-
-        $url = $host.":".$port."/fhem?cmd=".urlencode($this->parseReadCmd($item))."&XHR=1";
-
+        
+        $url = $host.":".$port."/fhem?cmd=".$cmd."&XHR=1";
+        
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $curl_response = curl_exec($curl);
@@ -119,11 +151,18 @@ class driver_json
             curl_close($curl);            
             return $curl_response;
         }
-    }
+    }   
     
     private function parseReadCmd($item)
-    {
+    {           
         return "{ReadingsVal('".$item."','state','')}";
+    }
+    
+    private function parseReadPlotCmd($item, $val)
+    {
+        $parsedItem = str_replace('->', '#', explode('.', $item)[0]);
+        $items = explode('#', $parsedItem);        
+        return "get ".$items[0]." - - ".$val["tmin"]." ".$val["tmax"]." ".$items[1];
     }
     
     private function parseWriteCmd($item, $val)
